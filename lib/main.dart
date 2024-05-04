c0650fd490656a877aa4452103222344a1cd72d1
ahmedeinshouka <76920911+ahmedeinshouka@users.noreply.github.com>
2024-05-04 04:22:05 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
     
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/splash',
 
       routes: {
         '/': (context) => home(),

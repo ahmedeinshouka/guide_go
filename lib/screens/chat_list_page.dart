6a828fa8f0f1139ce98cc358815de5f9b5c49d7c
ahmedeinshouka <76920911+ahmedeinshouka@users.noreply.github.com>
2024-05-13 03:50:36 +0300

    commit

--- lib/screens/chat_list_page.dart
+++ lib/screens/chat_list_page.dart
@@ -20,44 +20,77 @@ class ChatListPage extends StatelessWidget {
       ),
       body: UserList(),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
-        shadowColor: Colors.white,
-        elevation: 0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const SizedBox(width: 10),
-            IconButton(
-              highlightColor: Colors.amber,
-              onPressed: () {},
-              icon: const Icon(Icons.home),
-              iconSize: 40,
-            ),
-            const SizedBox(width: 20),
-            Container(color: Colors.black),
-            IconButton(
-              highlightColor: Colors.amber,
-              onPressed: () {},
-              icon: const Icon(Icons.people_alt_rounded, size: 40),
-            ),
-            const SizedBox(width: 20),
-            IconButton(
-              highlightColor: Colors.amber,
-              onPressed: () {},
-              icon: const Icon(Icons.favorite),
-              iconSize: 40,
-            ),
-            const SizedBox(width: 20),
-            IconButton(
-              highlightColor: Colors.amber,
-              onPressed: () {},
-              icon: const Icon(Icons.chat),
-              iconSize: 40,
-            ),
-            const SizedBox(width: 10),
-          ],
-        ),
-      ),
+          color: Colors.white,
+          shadowColor: Colors.white,
+          elevation: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                width: 10,
+              ),
+              IconButton(
+                  highlightColor: Colors.amber,
+                  onPressed: () {
+                    // Check if the current route is not the home screen
+                    if (ModalRoute.of(context)?.settings.name != '/') {
+                      Navigator.popAndPushNamed(context, "/");
+                    }
+                  },
+                  icon: const ImageIcon(
+                    AssetImage(
+                      "assets/icons8-home-page-32.png",
+                    ),
+                    size: 40,
+                  )),
+              const SizedBox(
+                width: 20,
+              ),
+              Container(
+                color: Colors.black,
+              ),
+              IconButton(
+                  highlightColor: Colors.amber,
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.people_alt_rounded,
+                    size: 40,
+                  )),
+              const SizedBox(
+                width: 10,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              IconButton(
+                  highlightColor: Colors.amber,
+                  onPressed: () {
+                    // Check if the current route is not the chatList screen
+                    if (ModalRoute.of(context)?.settings.name != '/chatList') {
+                      Navigator.pushNamed(context, "/chatList");
+                    }
+                  },
+                  icon: const ImageIcon(
+                    AssetImage("assets/chat.png"),
+                    size: 40,
+                  )),
+              const SizedBox(
+                width: 10,
+              ),
+              IconButton(
+                highlightColor: Colors.amber,
+                onPressed: () {
+                  // Check if the current route is not the profile screen
+                  if (ModalRoute.of(context)?.settings.name != '/profile') {
+                    Navigator.pushNamed(context, "/profile");
+                  }
+                },
+                icon: const Icon(Icons.person),
+                iconSize: 40,
+              ),
+            ],
+          ),
+        )
     );
   }
 }
